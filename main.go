6fc98dc7ae7e82cb236822c6eac9ebf8db34d7be
Alex Vidal <alex.vidal@gmail.com>
2018-11-29 12:02:15 -0600

    update

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main // import "code.heyviddy.com/import-test-repo"
 
-import "fmt"
+import (
+	"fmt"
+
+	_ "code.heyviddy.com/import-test-repo/sub1"
+)
 
 func main() {
 	fmt.Println("vim-go")
